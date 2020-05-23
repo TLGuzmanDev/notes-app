5e59b77b59725440d37f47cf56ea0fbebf423a32
Tony G <tlguzman94@gmail.com>
2020-05-23 14:35:49 -0400

    react frontend

---
+++ build/precache-manifest.1d956198516cd37c1cd695bbac202855.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b1e2de832940cd422ce2d80044d5c202",
+    "url": "/index.html"
+  },
+  {
+    "revision": "491eed0566bfda572f3b",
+    "url": "/static/css/main.87f3c5ff.chunk.css"
+  },
+  {
+    "revision": "2801e69fa28626424694",
+    "url": "/static/js/2.ec98fcbf.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ec98fcbf.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "491eed0566bfda572f3b",
+    "url": "/static/js/main.ee72fcb7.chunk.js"
+  },
+  {
+    "revision": "4f6b21ec2a0120c01f1b",
+    "url": "/static/js/runtime-main.da4820ff.js"
+  }
+]);
